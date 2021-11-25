547bb984730bce0f913aefa93cef68ab970f03a7
KLB <k.besser@tu-bs.de>
2021-11-25 11:18:46 +0100

    Add SVD precoding script and data transmission script

---
+++ data_transmission.py
@@ -0,0 +1,125 @@
+import logging
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+from scipy import stats
+from scipy import io
+
+from read_matrices import read_measurement_file, EVE, BOB, read_mat_file
+from util import RESULTS_DIR
+
+
+logging.basicConfig(format="%(asctime)s - [%(levelname)8s]: %(message)s")
+
+CMAP = "hot"
+CMAP = plt.get_cmap(CMAP)
+
+
+def read_data_file(data_file):
+    mat_dict = read_mat_file(data_file)
+    return mat_dict
+
+def expected_received_data(eff_channel, reception_matrix, data):
+    n = len(reception_matrix)
+    data = pack_data(data, n)
+    noise = np.random.rand(n) + 1j*np.random.rand(n)
+    #data = 10*data
+    #noise = 10*noise
+    received = data @ eff_channel + noise
+    received = received @ reception_matrix
+    received = unpack_data(received, n)
+    return received
+
+def pack_data(data, n=55, _packed_bits=2):
+    _data = np.reshape(data, (-1, _packed_bits))
+    _data = np.hstack((np.ones((len(_data), 1)), _data, np.zeros((len(_data), n-_packed_bits-1))))
+    assert np.shape(_data)[1] == n
+    return _data
+
+def unpack_data(received, n=55, _packed_bits=2):
+    if len(received) != n:
+        received = received.T
+    _received = received[1:_packed_bits+1, :] 
+    _received = np.ravel(_received, order="F")
+    _received = np.real(_received)
+    _received = np.abs(_received)
+    return _received
+
+def main(mat_file, data_file):
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+
+    _matrices = read_measurement_file(mat_file, precoded=False)
+    mat_bob = _matrices[BOB]
+    mat_eve = _matrices[EVE]
+    n = len(mat_bob)
+
+    U, S, Vh = np.linalg.svd(mat_bob)
+    _normalization_factor = 1./np.max(S)
+    mat_bob = _normalization_factor * mat_bob
+    U, S, Vh = np.linalg.svd(mat_bob)
+
+    S = np.diag(S)
+    reception_matrix = np.conj(Vh).T
+    _mat_prec = read_measurement_file(mat_file, precoded=True)
+    eff_mat_bob = _mat_prec[BOB]
+    eff_mat_eve = _mat_prec[EVE]
+    eff_mat_bob = eff_mat_bob @ Vh #np.conj(Vh).T  # saved matrix is U^H @ Bob @ V
+    inv_eff_mat_eve = np.linalg.inv(eff_mat_eve)
+
+    mat_data = read_data_file(data_file)
+    #plt.matshow(mat_data["transmitted_image"])
+    rec_bob_post = mat_data["y_Bob_after_decoding"]
+    _rec_data_bob = rec_bob_post[1:3, :] 
+    _rec_data_bob = np.ravel(_rec_data_bob, order="F")
+    assert np.all(_rec_data_bob == np.ravel(mat_data["data_received"]))
+    #_rec_data_bob = np.round(_rec_data_bob)
+    #_rec_data_bob = np.where(_rec_data_bob > .4, 1, 0)
+    _rec_image_bob = np.reshape(_rec_data_bob, (30, 30), order="F")
+
+    rec_eve = mat_data["y_Eve"]
+    rec_eve = rec_eve.T @ inv_eff_mat_eve
+    rec_eve = rec_eve.T
+    #rec_eve = inv_eff_mat_eve @ rec_eve
+    #_rec_data_eve = rec_eve[1:3, :] 
+    #_rec_data_eve = np.ravel(_rec_data_eve, order="F")
+    #_rec_data_eve = np.real(_rec_data_eve)
+    _rec_data_eve = unpack_data(rec_eve)
+    _rec_image_eve = np.reshape(_rec_data_eve, (30, 30), order="F")
+
+    #expected = expected_received_data(np.eye(55), np.eye(55), mat_data["data"])
+    #expected = expected_received_data(eff_mat_bob, reception_matrix, mat_data["data"])
+    expected = expected_received_data(eff_mat_eve, inv_eff_mat_eve, mat_data["data"])
+    expected_image = np.reshape(expected, (30, 30), order="F")
+    #plt.matshow(expected_image)
+    #plt.colorbar()
+
+    #fig, axs = plt.subplots(2, 2)
+    fig = plt.figure() # constrained_layout=True
+    gs = GridSpec(2, 2, figure=fig)
+    _im_orig = fig.add_subplot(gs[0, :]).matshow(mat_data["transmitted_image"], cmap=CMAP)
+    _im_bob = fig.add_subplot(gs[1, 0]).matshow(_rec_image_bob, cmap=CMAP)
+    _im_eve = fig.add_subplot(gs[1, 1]).matshow(_rec_image_eve, cmap=CMAP)
+
+    os.makedirs(RESULTS_DIR, exist_ok=True)
+    plt.imsave(os.path.join(RESULTS_DIR, "image_original.pdf"),
+               mat_data["transmitted_image"], cmap=CMAP)
+    plt.imsave(os.path.join(RESULTS_DIR, "image_bob.pdf"), _rec_image_bob,
+               cmap=CMAP)
+    plt.imsave(os.path.join(RESULTS_DIR, "image_eve.pdf"), _rec_image_eve,
+               cmap=CMAP)
+    return mat_data
+
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    #parser.add_argument("-k", type=int, help="Max number of used modes", default=2)
+    parser.add_argument("mat_file", help="Mat-file with matrices")
+    parser.add_argument("data_file", help="Mat-file with transmitted data")
+    args = vars(parser.parse_args())
+    results = main(**args)
+    plt.show()
---
+++ svd_precoding.py
@@ -0,0 +1,203 @@
+import logging
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy import io
+
+from read_matrices import read_measurement_file, EVE, BOB
+from util import setup_logging_config, generate_data, save_results, RESULTS_DIR
+from waterfilling import water_filling
+
+# Tse 2005 page 294
+
+#logging.basicConfig(format="%(asctime)s - [%(levelname)8s]: %(message)s")
+np.random.seed(20210716)
+
+
+def capac_bsc(bit_flip_prob):
+    return 1.-stats.bernoulli.entropy(p=bit_flip_prob)/np.log(2)
+
+def calc_power_waterfilling(sing_values, power):
+    #inv_sing = 1./sing_values**2
+    #mu = (power + sum(inv_sing))/len(inv_sing)
+    #print(mu)
+    #power_vector = np.maximum(mu-inv_sing, 0)
+    alpha_i = 1/sing_values**2
+    #_status, _value, power_vector = water_filling(len(alpha_i), alpha_i, sum_x=power)
+    power_vector = water_filling(alpha_i, power)
+    return power_vector
+
+
+#def calc_bit_flip_prob(sing_values, power_vec):
+def calc_bit_flip_prob(noise_trans_matrix, power_vec):
+    noise_var = np.real(noise_trans_matrix @ np.conj(noise_trans_matrix).T)
+    q_val = stats.norm.sf(power_vec/(np.sqrt(np.diag(noise_var))))
+    return q_val
+
+
+def main(snr, n=3, k=1, matrix=None, precoded=False, num_samples=100000):
+    os.makedirs(RESULTS_DIR, exist_ok=True)
+    _prefix = "SVD"
+    if matrix is not None:
+        _matrices = read_measurement_file(matrix, precoded=False)
+        _basename = os.path.splitext(os.path.basename(matrix))[0]
+        if precoded:
+            dirname = "{}-{}-precoded".format(_prefix, _basename)
+        else:
+            dirname = "{}-{}".format(_prefix, _basename)
+        mat_bob = _matrices[BOB]
+        mat_eve = _matrices[EVE]
+        n = len(mat_bob)
+    else:
+        np.random.seed(100)
+        #matrices = {BOB: np.array([[.77, -.3], [-.32, -.64]]),
+        #            EVE: np.array([[.54, -.11], [-.93, -1.71]])}
+        #mat_bob = stats.expon.rvs(size=(n, n), scale=.1)
+        mat_bob = np.random.randn(n, n) + 1j*np.random.randn(n, n)
+        #mat_bob = np.eye(n)
+        mat_eve = np.random.randn(n, n) + 1j*np.random.randn(n, n)
+        dirname = "{0}-{1}x{1}".format(_prefix, n)
+
+    dirname = os.path.join(RESULTS_DIR, dirname)
+    os.makedirs(dirname, exist_ok=True)
+    setup_logging_config(dirname)
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.INFO)
+
+    U, S, Vh = np.linalg.svd(mat_bob)
+    _normalization_factor = 1./np.max(S)
+
+    mat_bob = _normalization_factor * mat_bob
+    U, S, Vh = np.linalg.svd(mat_bob)
+
+    S = np.diag(S)
+    reception_matrix = np.conj(Vh).T
+    if precoded:
+        #reception_matrix = np.identity(n)
+        _mat_prec = read_measurement_file(matrix, precoded=True)
+        eff_mat_bob = _mat_prec[BOB]
+        eff_mat_eve = _mat_prec[EVE]
+        plt.matshow(np.abs(eff_mat_bob))
+        eff_mat_bob = eff_mat_bob @ Vh #np.conj(Vh).T  # saved matrix is U^H @ Bob @ V
+    else:
+        #_svd_noise = stats.uniform.rvs(loc=0,
+        #                            #scale=3/(0.5*np.arange(n)+1),
+        #                            scale=np.linspace(1., .01, n)*2,
+        #                            size=np.shape(mat_bob))
+        #_svd_noise[np.diag_indices_from(_svd_noise)] = 0.
+        #_mat_bob = np.copy(mat_bob)
+        #mat_bob = mat_bob + U @ _svd_noise @ Vh
+        #io.savemat(os.path.join(dirname, "rolle01.mat"), {"clean": _mat_bob, "noisy": mat_bob})
+        precoding_matrix = np.conj(U).T
+        #precoding_matrix = _normalization_factor * precoding_matrix
+        #reception_matrix = np.conj(Vh).T
+        #reception_matrix = _normalization_factor * reception_matrix
+        eff_mat_bob = precoding_matrix @ mat_bob
+        eff_mat_eve = precoding_matrix @ mat_eve
+        plt.matshow(np.abs(precoding_matrix @ mat_bob @ reception_matrix))
+    #inv_eff_mat_bob = np.linalg.inv(eff_mat_bob)
+    #reception_matrix = inv_eff_mat_bob
+    inv_eff_mat_eve = np.linalg.inv(eff_mat_eve)
+
+    plt.colorbar()
+    plt.show()
+
+    #_one = np.zeros((3, n))
+    #_one[0, 0] = 1
+    #_one[1, 1] = 1
+    #_one[2, 2] = 1
+    #plt.matshow(np.abs(_one@eff_mat_bob@reception_matrix))
+    #plt.matshow(np.abs(eff_mat_bob@reception_matrix))
+    #plt.colorbar()
+    #plt.show()
+    #raise
+
+    power = 10**(snr/10.)
+    logger.info("SNR: %f dB", snr)
+    logger.debug("Power constraint: %f", power)
+
+    results = {"k": [], "secCapacMC": [], "secCapacTheo": []}
+    for _k in range(1, k+1):
+        logger.info("Number of modes: %d", _k)
+        sing_val = np.diag(S)[:_k]
+        logger.debug("Singular values: %s", sing_val)
+        _power_vec_wf = calc_power_waterfilling(sing_val, power)
+        power_vec_wf = np.zeros(n)#np.zeros_like(S)
+        power_vec_wf[:_k] = _power_vec_wf
+        #logger.debug(power_vec_wf)
+        logger.debug("Sum power allocated vs available power: {:.3f}/{:.3f}".format(sum(power_vec_wf), power))
+
+        # Monte Carlo Simulations
+        messages = generate_data(_k, num_samples, mod="bpsk")
+        tx_symbols = np.zeros((num_samples, n))
+        tx_symbols[:, :_k] = _power_vec_wf * messages
+        #tx_symbols[:, :_k] = np.expand_dims(_power_vec_wf, 1) * messages
+        noise_bob = np.random.randn(num_samples, n) + 1j*np.random.randn(num_samples, n)
+        noise_eve = np.random.randn(num_samples, n) + 1j*np.random.randn(num_samples, n)
+        rec_bob = tx_symbols @ eff_mat_bob + noise_bob
+        rec_eve = tx_symbols @ eff_mat_eve + noise_eve
+        est_bob = rec_bob @ reception_matrix
+        est_eve = rec_eve @ inv_eff_mat_eve
+        est_mess_bob = np.real(np.sign(est_bob[:, :_k]))
+        est_mess_eve = np.real(np.sign(est_eve[:, :_k]))
+
+        #bit_flip_bob = np.count_nonzero(est_mess_bob != messages, axis=1)/num_samples
+        bit_flip_bob = np.count_nonzero(est_mess_bob != messages, axis=0)/num_samples
+        bit_flip_eve = np.count_nonzero(est_mess_eve != messages, axis=0)/num_samples
+        capac_bob_mc = capac_bsc(bit_flip_bob)
+        capac_eve_mc = capac_bsc(bit_flip_eve)
+        _sec_capac_mc = np.sum(np.maximum(capac_bob_mc-capac_eve_mc, 0))
+        logger.debug("MC Bit Flip Bob: %s", bit_flip_bob)
+        logger.debug("MC Bit Flip Eve: %s", bit_flip_eve)
+        #logger.debug("BSC Capac Bob: %s", capac_bob_mc)
+        #logger.debug("BSC Capac Eve: %s", capac_eve_mc)
+        #logger.debug("Differences: %s", np.maximum(capac_bob_mc-capac_eve_mc, 0))
+        logger.info("Secrecy Capacity: %f", _sec_capac_mc)
+        results["secCapacMC"].append(_sec_capac_mc)
+
+        # Theoretical
+        #bit_flip_prob_bob = calc_bit_flip_prob(np.diag(1./S), power_vec_wf)
+        bit_flip_prob_bob = calc_bit_flip_prob(np.linalg.inv(eff_mat_bob @ reception_matrix), power_vec_wf)
+        bit_flip_prob_eve = calc_bit_flip_prob(inv_eff_mat_eve, power_vec_wf)
+        #logger.info(bit_flip_prob_bob)
+        #logger.info(bit_flip_prob_eve)
+
+        capac_bob = capac_bsc(bit_flip_prob_bob)
+        capac_eve = capac_bsc(bit_flip_prob_eve)
+        _sec_capac = np.maximum(capac_bob - capac_eve, 0)
+        sec_capac = sum(_sec_capac)
+        logger.debug("Theo Bit Flip Bob: %s", bit_flip_prob_bob)
+        logger.debug("Theo Bit Flip Eve: %s", bit_flip_prob_eve)
+        logger.info("Secrecy capacity: %f", sec_capac)
+        results["k"].append(_k)
+        results['secCapacTheo'].append(sec_capac)
+
+    save_results(dirname, results, snr)
+
+    if not precoded:
+        plt.plot(results['k'], results['secCapacTheo'], 'o-', label="Theoretical Values")
+    plt.plot(results['k'], results['secCapacMC'], '^-', label="Monte Carlo Simulation")
+    plt.xlabel("Number of used modes $|\mathcal{K}|$")
+    plt.ylabel("Secrecy Capacity [bit]")
+    plt.legend()
+    plt.tight_layout()
+    _plot_fn = "results-snr{}.png".format(snr)
+    plt.savefig(os.path.join(dirname, _plot_fn), dpi=200)
+    #plt.matshow(np.abs(np.conj(Vh).T))
+    #logger.info("Maximum absolute value in precoding matrix: %f", np.max(np.abs(np.conj(Vh).T)))
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", type=int, help="Number of modes", default=4)
+    parser.add_argument("-k", type=int, help="Max number of used modes", default=2)
+    parser.add_argument("-s", "--snr", type=float, help="SNR", default=10)
+    parser.add_argument("--matrix", help="Mat-file with matrices")
+    parser.add_argument("--num_samples", type=int, default=100000, help="Number of Monte Carlo samples")
+    parser.add_argument("--precoded", action="store_true")
+    args = vars(parser.parse_args())
+    main(**args)
+    plt.show()
