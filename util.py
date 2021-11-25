bb4b21de8265ff45ea36b665ab16ff1df1c126e6
KLB <k.besser@tu-bs.de>
2021-11-25 11:19:20 +0100

    Add module with utility functions

---
+++ util.py
@@ -0,0 +1,48 @@
+import logging
+from logging.config import dictConfig
+import os
+
+import numpy as np
+import pandas as pd
+
+
+RESULTS_DIR = "results"
+
+
+def setup_logging_config(dirname, level=logging.DEBUG):
+    logging_config = dict(
+        version = 1,
+        formatters = {
+                      'f': {'format': "%(asctime)s - [%(levelname)8s]: %(message)s"}
+                     },
+        handlers = {'console': {'class': 'logging.StreamHandler',
+                                'formatter': 'f',
+                                'level': level,
+                               },
+                    'file': {'class': 'logging.FileHandler',
+                             'formatter': 'f',
+                             'level': logging.DEBUG,
+                             'filename': os.path.join(dirname, "main.log")
+                            },
+                   },
+        loggers = {"main": {'handlers': ['console', 'file'],
+                            'level': logging.DEBUG,
+                           },
+                   "algorithm": {'handlers': ['console', 'file'],
+                                 'level': logging.DEBUG,
+                                },
+                  }
+        )
+    dictConfig(logging_config)
+
+def generate_data(num_streams, num_samples, mod="bpsk"):
+    mod = mod.lower()
+    if mod == "bpsk":
+        data = np.random.choice([-1, 1], size=(num_samples, num_streams))
+    else:
+        raise NotImplementedError
+    return data
+
+def save_results(dirname, results, snr):
+    dat_file = os.path.join(dirname, "capac_results-snr{}.dat".format(snr))
+    pd.DataFrame.from_dict(results).to_csv(dat_file, sep='\t', index=False)
