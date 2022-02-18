0ba56bce0492a1fca9cb08baa430f98cebc06ffd
KLB <k.besser@tu-bs.de>
2022-02-18 11:36:49 +0100

    Improve logging in statistics plot script

--- show_capacity_statistics.py
+++ show_capacity_statistics.py
@@ -1,4 +1,5 @@
 import os.path
+import logging
 
 import numpy as np
 import pandas as pd
@@ -6,6 +7,7 @@ import matplotlib.pyplot as plt
 
 from util import RESULTS_DIR, RESULTS_DATA
 
+LOGGER = logging.getLogger(__name__)
 
 def main(snr, option=0):# precoded=False):
     """
@@ -31,15 +33,13 @@ def main(snr, option=0):# precoded=False):
         data = np.array(data.get("secCapacMC", data.get("secCapac")))
         results.append(data)
     results = np.dstack(results)
-    print(results.shape)
     average = np.ravel(np.mean(results, axis=-1))
     std = np.ravel(np.std(results, axis=-1, ddof=1))
     quant_low = np.ravel(np.quantile(results, .00, axis=-1))
     quant_high = np.ravel(np.quantile(results, 1., axis=-1))
-    print(average)
-    print(std)
-    print(quant_low)
-    print(quant_high)
+    LOGGER.info("Average secrecy rate (for the first 5 modes):\t{}".format(average[:5]))
+    LOGGER.info("Minimum secrecy rate:\t{}".format(quant_low[:5]))
+    LOGGER.info("Maximum secrecy rate:\t{}".format(quant_high[:5]))
     #plt.errorbar(range(len(average)), average, yerr=std)
     k = range(len(std))
     plt.figure()
@@ -50,10 +50,25 @@ def main(snr, option=0):# precoded=False):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(asctime)s - [%(levelname)8s]: %(message)s",
+                        handlers=[
+                            logging.FileHandler("main.log", encoding="utf-8"),
+                            logging.StreamHandler()
+                        ])
+    LOGGER.setLevel(logging.INFO)
     snr = 10.
+    LOGGER.debug(f"Using SNR: {snr} dB")
+
+    LOGGER.info("Upper Bound (Perfect SVD at Alice)")
     avg_main, std_main, low_main, high_main = main(snr, option=0)
+
+    LOGGER.info("Real Implemented SVD at Alice")
     avg_prec, std_prec, low_prec, high_prec = main(snr, option=1)
+
+    LOGGER.info("No Precoding at Alice")
     avg_nop, std_nop, low_nop, high_nop = main(snr, option=2)
+
+    LOGGER.info("Gain of precoding (real system vs no precoding: {:.1f}".format(max(avg_prec)/max(avg_nop)))
     results_file = os.path.join(RESULTS_DIR, "capac-statistic-{}.dat".format(snr))
     pd.DataFrame.from_dict({"k": np.arange(1, len(avg_main)+1),
                             "avg": avg_main, "std": std_main,
